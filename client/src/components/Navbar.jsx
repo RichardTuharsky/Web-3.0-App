78d17e348e05655fd060f7a4face914b964a5b7f
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-01-31 19:25:39 +0100

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.svg';
+
+
+
+
+const Navbar = () => {
+    return (
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+            
+         </nav>
+    );
+}
+
+export default Navbar;
